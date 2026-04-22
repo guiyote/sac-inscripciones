910857e237f90e01bae6706acd85cfeea9e0e97a
guiyote <guiyote@gmail.com>
2026-04-22 15:56:29 -0300

    fix labels

---
+++ sql/supabase_update_articulos.sql
@@ -0,0 +1,3 @@
+-- Actualizar nombres de artículos en stock
+UPDATE stock_articulos SET nombre = 'Discos de freno (par)' WHERE nombre = 'Discos de freno';
+UPDATE stock_articulos SET nombre = 'Pastillas (caja)' WHERE nombre = 'Pastillas';
